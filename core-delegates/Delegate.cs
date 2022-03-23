8f0332427ef6ad0f89ba3e727b664d759d85d526
Kanhaiya Tyagi <kanhaiyatyagi63@gmail.com>
2022-03-23 15:52:54 +0530

    Add project files.

---
+++ core-delegates/Delegate.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace core_delegates
+{
+    delegate int NumberChanger(int number);
+
+    public class Delegate
+    {
+        static int sum = 1;
+
+        public static int Addition(int value)
+        {
+            sum += value;
+            return sum;
+        }
+        public static int Multiply(int value)
+        {
+            sum *= value;
+            return sum;
+        }
+        public static int GetSum()
+        {
+            return sum;
+        }
+
+        public static string AdditionOfTwoNumber(int value1, int value2)
+        {
+            return (value1 + value2).ToString();
+        }
+
+        public static bool IsGreaterThanTen(int value)
+        {
+            return value > 10;
+        }
+        public static void PrintAction(string value1, string value2)
+        {
+            Console.WriteLine($"{value1} {value2}");
+        }
+        public static void Seperator()
+        {
+            Console.WriteLine("\n-----------------------------------");
+        }
+    }
+}
---
+++ core-delegates/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace core_delegates
+{
+    // delegates
+    // 1. Single cast 
+    // 2. Multi cast
+    // 3. Generic
+    // 3.a Action => no return 
+    // 3.b Predicate => only return boolean value
+    // 3.c Func => return anything that you want from the function
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello Delegates!");
+
+            NumberChanger globalDelegate = null;
+            var additionNumberChanger = new NumberChanger(Delegate.Addition);
+            var multiplyNumberChanger = new NumberChanger(Delegate.Multiply);
+
+            Delegate.Seperator();
+
+            #region single cast delegate
+            additionNumberChanger(5);
+            Console.WriteLine($"The sum is: {Delegate.GetSum()}");
+            multiplyNumberChanger(5);
+            Console.WriteLine($"The sum is: {Delegate.GetSum()}");
+            #endregion
+
+            Delegate.Seperator();
+
+            #region multi cast delegate
+
+            globalDelegate = multiplyNumberChanger;
+            globalDelegate += additionNumberChanger;
+            globalDelegate(5);
+
+            Console.WriteLine($"The sum is: {Delegate.GetSum()}");
+
+            #endregion
+
+            Delegate.Seperator();
+
+            #region Generic 1. Func Delegate
+            // Syntax: Func<int, string, string> VariableName = reference
+            // Func no paramter restriction, get any type of output that you want
+            Func<int, int, string> Addition = Delegate.AdditionOfTwoNumber;
+            var funcResult = Addition(10, 15);
+            Console.WriteLine($"Func Delegate: Sum is: {funcResult}");
+            #endregion
+            Delegate.Seperator();
+
+            #region Generic 2. Predicate Delegate
+            // Syntax: Predicate<T> VariableName = reference
+            // Func one paramter restriction, get only boolean type output
+            int value = 9;
+            Predicate<int> isGreaterThan = Delegate.IsGreaterThanTen;
+            bool predresult = isGreaterThan(value);
+            Console.WriteLine($"{value} is greater than 10: {predresult}");
+            #endregion
+            Delegate.Seperator();
+
+            #region Generic 3. Action Delegate
+            // Syntax: Action<int, string> VariableName = reference
+            // Func no paramter restriction, no return
+            Action<string, string> print = Delegate.PrintAction;
+            print("Welcome!","I am a action delegate");
+            #endregion
+        }
+    }
+}
